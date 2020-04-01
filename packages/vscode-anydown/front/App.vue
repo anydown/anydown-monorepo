d2264e3da2fbc3f91d553ea7661da0fa27873c1e
hashrock <hashrock@users.noreply.github.com>
2020-04-02 03:10:19 +0900

    feat: add buttons

--- packages/vscode-anydown/front/App.vue
+++ packages/vscode-anydown/front/App.vue
@@ -1,11 +1,37 @@
 <template>
-  <anydown :blocks="splited" @change="updateBlock($event)"></anydown>
+  <div>
+    <anydown :blocks="splited" @change="updateBlock($event)"></anydown>
+    <div>
+      <span class="label-add">
+        Add
+      </span>
+      
+      <button class="add" @click="addKanban">Kanban</button>
+      <button class="add" @click="addGantt">Gantt</button>
+      <button class="add" @click="addBlock">BlockDiag</button>
+      <button class="add" @click="addCsv">SpreadSheet</button>
+    </div>
+
+  </div>
 </template>
 <style>
 body{
   background: white;
   color: black;
   font-size: 16px;
+}
+.label-add{
+  color: #499be3;
+  font-size: 0.8rem;
+}
+button.add{
+  border: none;
+  border-radius: 2rem;
+  background: #DDD;
+  font-weight: 700;
+}
+button.add:hover{
+  background: #AAF;
 }
 </style>
 <script>
@@ -29,6 +55,18 @@ export default {
     }
   },  
   methods: {
+    addKanban(){
+      this.input += "\n```kanban\n# TODO\n# DONE\n```\n" 
+    },
+    addGantt(){
+      this.input += "\n```gantt\n```\n" 
+    },
+    addBlock(){
+      this.input += "\n```block\n\n```\n" 
+    },
+    addCsv(){
+      this.input += "\n```csv\n,,,\n,,,\n,,,\n```\n" 
+    },
     updateBlock(payload) {
       this.splited[payload.id].text = payload.type + "\n" + payload.body;
       this.input = this.splited.map(i => i.text).join("```");
